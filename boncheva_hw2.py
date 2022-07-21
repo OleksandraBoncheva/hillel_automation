cfa37c792aa79b7eaa5f589d93fe718ebb36349c
Oleksandra Boncheva <aleksandraboncheva1997@gmail.com>
2022-07-21 21:09:19 +0300

    Add HW3

---
+++ boncheva_hw2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Assignment 01-01
+#
+# Assign actual data to the variables below
+# and run the script to print the output.
+
+name = "Alex"
+profession = "QA Manual"
+facility = "Sprinterra"
+city = "Odesa"
+country = "Ukraine"
+goal = "software engineer"
+age = 25
+
+print(
+f"""
+My name is {name}. I am a {profession} at {facility} 
+in {city}, {country}. I want to learn how to program
+in Python to become a {goal}. I am {age} years old.
+"""
+) 
