7f111befa47ca57a59d9885c38d514b151c7c04a
Oleksandra Boncheva <35664065+OleksandraBoncheva@users.noreply.github.com>
2022-08-11 19:45:42 +0300

    Update assignment07_03.py

--- assignment07_03.py
+++ assignment07_03.py
@@ -11,16 +11,7 @@
 N = int(input("N = "))
 K = int(input("K = "))
 
-data = [i if K > N:
-    K %= N for i in range(N) i % K != 0]
+list = [K for K in range(N)]
 del list[::K]
 
 print(list)
-
-if word in counts1:
-    counts1[word] += 1
-else:
-    counts1[word] = 1
-    counts2 = dict()
-    for i in input_str:
-        counts2[i] = counts2.get(i, 0) + 1
